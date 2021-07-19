758e1cded77ae462c596a07950821b813a8d8b70
phonqhoanq <phong.ht144@gmail.com>
2021-07-19 08:40:20 +0700

    install prettier,husky,lint-staged

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,13 @@
 class NewsController {
-
     //GET /news
     index(req, res) {
-        res.render('news')
+        res.render('news');
     }
     show(req, res) {
-        res.send('How are uu')
+        res.send('How are uu');
     }
 }
 
-module.exports = new NewsController
+module.exports = new NewsController();
 
-const newsController = require('./NewsController')
+const newsController = require('./NewsController');
